58735514b17c1e5bc785e2b695f5c4b362cbf9b2
yaraelzein <yaraelzein2001@gmail.com>
2022-04-15 16:12:27 -0500

    prueba1 de publicacion hacia npm

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-module.exports = () => {
-  // ...
-};
+module.exports = {
+  mdLinks: (path, options) => {
+    console.log(`Este es el path: ${path}\nEstas son las mañanitas ${options}`);
+  }
+}
---
+++ markdown-examples.md/example1.md
@@ -0,0 +1,3 @@
+[Google](https://www.google.com/)
+[algun documento](https://otra-cosa.net/algun-doc.html)
+[Link a algo](http://algo.com/2/3/)

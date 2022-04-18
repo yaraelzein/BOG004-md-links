fab5949a68eec07a15a0dd07e168f8a62ce26231
yaraelzein <yaraelzein2001@gmail.com>
2022-04-18 18:19:33 -0500

    creacion de test para condicionales de validacion de tipo string

--- markdown-examples.md/example1.md
+++
@@ -1,3 +0,0 @@
-[Google](https://www.google.com/)
-[algun documento](https://otra-cosa.net/algun-doc.html)
-[Link a algo](http://algo.com/2/3/)
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,42 @@
 const mdLinks = require('../');
 
 
-describe('mdLinks', () => {
+describe('mdLinks path', () => {
+  it('debria ser un string', () => {
+    const spyConsole = jest.spyOn(console, 'log');
 
-  it('should...', () => {
-    console.log('FIX ME!');
+    const spyProcess = jest.spyOn(process, "exit").mockImplementation((number) => { 
+      throw new Error('process.exit: ' + number); 
+    });
+
+    expect(() => {
+      mdLinks(0)
+    }).toThrow();
+
+    expect(spyConsole.mock.calls).toEqual([["error. path debe ser un string"]]);
+    expect(spyProcess).toHaveBeenCalledWith(1);
+
+    spyConsole.mockRestore()
+    spyProcess.mockRestore()
   });
 
+  it('es un string vacio', () => {
+    const spyConsole = jest.spyOn(console, 'log');
+
+    const spyProcess = jest.spyOn(process, "exit").mockImplementation((number) => { 
+      throw new Error('process.exit: ' + number); 
+    });
+
+    expect(() => {
+      mdLinks('')
+    }).toThrow();
+
+    expect(spyConsole.mock.calls).toEqual([["error. path vacio"]]);
+    expect(spyProcess).toHaveBeenCalledWith(1);
+
+    spyConsole.mockRestore()
+    spyProcess.mockRestore()
+  });
+
+  
 });

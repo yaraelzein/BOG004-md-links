f589181b9127e0ba30aecbf23815a9893ff99ae6
yaraelzein <yaraelzein2001@gmail.com>
2022-05-14 14:10:53 -0500

    implementacion de promesa a la funcion md links

--- index.js
+++ index.js
@@ -1,59 +1,80 @@
 const { rejects } = require('assert');
-const fs = require('fs')
+const { error } = require('console');
+const fs = require('fs')//las funciones reciben un callback
+const http = require('http')
+const https = require('https')
+const { validateFalse, validateTrue, stats} = require('./src/funciones')
 
-module.exports = (path, options) => {
-  let mdFiles = [];
+module.exports = (path, options) => new Promise((resolve, reject) => {
+  let pathValidator = [];
+  let pwd = [];
+  let newPath = ""
+  
   //console.log(path, options) inicia ejecucion mdLikns
   if (typeof path != 'string') {
-    console.log('error. path debe ser un string')
-    process.exit(1)
+    reject("Error. path deberia ser un string")
   }
-
+  
   if (path === ''){
-    console.log('error. path vacio')
-    process.exit(1)
+    reject('Error. path vacio')
+  }
+  
+  //es realtivo o abosluto
+  pathValidator = path.split("/");
+  
+  if (pathValidator[0] === "."){
+    pwd = __dirname.split("/");
+    // remove the first element in pathValidator & pwd
+    pathValidator.shift();
+    pwd.shift();
+    // create path absolute
+    newPath = "/".concat(pwd.concat(pathValidator).join("/"))
   }
 
-  if (path.match(".+(.md)") == null){
-    //si la ruta no es directa, ejemplo: ./path
-    //validar si podemos ver el directorio
-    fs.access(path, (error) => {
-      if (error) {
-        console.log("la ruta NO existe")
-        process.exit(1)
+  //llamar funcion: existe directorio
+  existDir(newPath).then(
+    promiseValue => {
+      if (promiseValue == 0); {
+        reject('Error. No existe el directorio')
       }
-    })
-
-    // let readdirPromesa = new Promise((resolve, reject) => {
-
-    // })
-    fs.readdir(path, (error, files) => {
-      if (error) {
-        console.log("No se logro leer el directorio")
-      } else {
-        files.forEach((file) => {
-          if (file.match(".+(.md)") != null){
-            mdFiles.push("/" + file)
-          }
-        })
-      }
-    })
-    setTimeout(function(){
-      console.log(mdFiles)
-    }, 250);
-  }
-  //si la ruta es directa, ejemplo: ./path/file.md
-  if(mdFiles.length != 0){
-    console.log("tiene link");
-  }
-  console.log(path + "/" + mdFiles)
-  fs.readFile(path, 'utf-8', (error, data) => {
-    if (error) {
-      console.log('error data')
-    } else {
-      console.log(data)
     }
-  })
+  )
 
-  console.log("paso")
-}
+  if (options['stats'] == true){
+    resolve(stats(path, options['validate']))
+  }
+
+  if (options['validate'] == true){
+    validateTrue(path).then(
+      value => {
+        resolve(value)
+      }
+    )
+  }
+  else
+  {
+    validateFalse(path).then(
+      value => {
+        resolve(value)
+      }
+    )
+  }
+})
+
+
+//funcion: existe directorio
+    const existDir = (path) => {
+      return new Promise (function (resolve) {
+        fs.stat(path, (err, stats) => {
+          if(err) {
+            console.log(error);
+          }
+          else {
+            console.log('stat object for: path');
+            // console.log(stats);
+          } 
+        })
+      })
+    }
+//
+

83e8706cf32ab58b0df88ede32ba1dea4a8f2166
yaraelzein <yaraelzein2001@gmail.com>
2022-05-23 09:40:32 -0500

    creacion de test

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,60 +1,77 @@
 const mdLinks = require('../');
 
+// test path invalido
+const invalidPath = './markdown-examples/example1.m'
+const errorPath = 'error. path no existe'
 
-describe('mdLinks path', () => {
-  it('debria ser un string', () => {
-    const spyConsole = jest.spyOn(console, 'log');
+// test salida valida del path
+const validPath = './markdown-examples/example1.md'
+const validOutput = [
+  './markdown-examples/example1.md https://www.google.com/ Google',
+  './markdown-examples/example1.md https://otra-cosa.net/algun-doc.html algun documento',
+  './markdown-examples/example1.md http://algo.com/2/3/ Link a algo'
+]
 
-    const spyProcess = jest.spyOn(process, "exit").mockImplementation((number) => { 
-      throw new Error('process.exit: ' + number); 
-    });
+//path vacio
+const errPathvacio = 'Error. path vacio'
 
-    expect(() => {
-      mdLinks(0)
-    }).toThrow();
+//test validate true
+const validateFlag = './markdown-examples/example1.md'
+const validateFlagValue = [
+  './markdown-examples/example1.md https://www.google.com/ ok 200 Google',
+  './markdown-examples/example1.md https://otra-cosa.net/algun-doc.html fail 400 algun documento',
+  './markdown-examples/example1.md http://algo.com/2/3/ ok 302 Link a algo'
+]
 
-    expect(spyConsole.mock.calls).toEqual([["error. path debe ser un string"]]);
-    expect(spyProcess).toHaveBeenCalledWith(1);
+//test stats true
+const statsFlag = './markdown-examples/example1.md'
+const statsFlagValue = { Total: 3, unique: 3 }
 
-    spyConsole.mockRestore()
-    spyProcess.mockRestore()
+//test stats & validate true
+const sVFlags = './markdown-examples/example1.md'
+const sVFlagsValue = { Total: 3, unique: 3, broken: 1 }
+
+//---------------------------------------------------------------------------------------------------------
+describe('mdLinks', () => {
+
+  it('Es una funcion', () => {
+    expect(typeof mdLinks).toBe('function');
   });
 
-  it('es un string vacio', () => {
-    const spyConsole = jest.spyOn(console, 'log');
-
-    const spyProcess = jest.spyOn(process, "exit").mockImplementation((number) => { 
-      throw new Error('process.exit: ' + number); 
-    });
-
-    expect(() => {
-      mdLinks('')
-    }).toThrow();
-
-    expect(spyConsole.mock.calls).toEqual([["error. path vacio"]]);
-    expect(spyProcess).toHaveBeenCalledWith(1);
-
-    spyConsole.mockRestore()
-    spyProcess.mockRestore()
+  it('Path invalido', () => {
+    return mdLinks(invalidPath, {'validate': false}).catch((e) => {
+      expect(e).toMatch(errorPath)
+    })
   });
-
-  it('existe directorio', () => {
-    const spyConsole = jest.spyOn(console, 'log');
-
-    const spyProcess = jest.spyOn(process, "exit").mockImplementation((number) => { 
-      throw new Error('process.exit: ' + number); 
-    });
-
-    expect(() => {
-      mdLinks('./markdown')
-    }).toThrow();
-
-    expect(spyConsole.mock.calls).toEqual([["error. no existe el directorio"]]);
-    expect(spyProcess).toHaveBeenCalledWith(1);
-
-    spyConsole.mockRestore()
-    spyProcess.mockRestore()
-  })
-
   
+  it('path valido', () => {
+    return mdLinks(validPath, {'validate': false}).then((valid) => {
+      expect(valid).toEqual(validOutput)
+    })
+  });
+
+  it('path vacio', () => {
+    return mdLinks('', {'validate': false}).catch((err) => {
+      expect(err).toMatch(errPathvacio)
+    });
+  });
+
+  it ('validate flag true', () => {
+    return mdLinks(validateFlag, {'validate': true}).then((valid) => {
+      expect(valid).toEqual(validateFlagValue)
+    });
+  });
+
+  it('stats flag true', () => {
+    return mdLinks(statsFlag, {'stats': true, 'validate': false}).then((valid) => {
+      expect(valid).toEqual(statsFlagValue)
+    });
+  });
+
+  it('stats & validate flags true', () => {
+    return mdLinks(sVFlags, {'stats': true, 'validate': true}).then((valid) => {
+      expect(valid).toEqual(sVFlagsValue)
+    });
+  });
+
 });

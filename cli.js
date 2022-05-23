29a9b85c0f2bbee41f320384ab375a9935f096c1
yaraelzein <yaraelzein2001@gmail.com>
2022-05-23 09:16:34 -0500

    creacion de funcion cli + construccion de flags

--- cli.js
+++ cli.js
@@ -1,9 +1,28 @@
-const mdLinks = require("./index");
+#!/usr/local/bin/node
+const mdLinks = require('./index');
 const process = require('process');
+const chalk = require('chalk');
+
+const cli = () => {
+    const [, , path, posicion1, posicion2] = process.argv;
+    let isValid = false
+    let stats = false
+
+    if(posicion1 == '--validate' || posicion2 == '--validate'){
+        isValid = true
+    }
+
+    if(posicion1 == '--stats' || posicion2 == '--stats'){
+        stats = true
+    }
 
 
-mdLinks("./markdown-examples/example1.md", {'validate': true, 'stats': true})
-    .then((response)=> {
-        console.log(response);
-    })
-    .catch((err) => console.log(err));
+
+    mdLinks(path, {'validate': isValid, 'stats': stats})
+        .then((response)=> {
+            console.log(response);
+        })
+        .catch((err) => console.log(err));
+}
+
+cli()
